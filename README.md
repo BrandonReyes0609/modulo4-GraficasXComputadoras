50faff63299d78d4db62fbd9e2b49fcff0a3bd5d
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-10-20 11:36:21 -0600

    mcambios 6Orbit_camera_2

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
 #Modulo 4 
 
 ## Códigos importantes
-cargo run
+```cargo run```
 
-cargo build --release
+```cargo build --release```
 
-convert output.ppm outputIMG.png
+```convert output.ppm outputIMG.png```
+```magick output.ppm outputIMG.png```
 
 ## Códigos importantes repositorio-githusb
 1. Crear la nueva rama
-git checkout -b NRAMA
+```git checkout -b NRAMA```
 2. Añadir todos los archivos
-git add .
+```git add .```
 3. Realizar el commit
-git commit -m "mcambios NRAMA"
+```git commit -m "mcambios NRAMA"```
 4. Subir la nueva rama al repositorio remoto
-git push origin NRAMA
+```git push origin NRAMA```
+
+
+## Otros comandos
+1. Ver rama actual
+```git branch```
+2. Ver todas las ramas locales
+```git branch```
+3. Ver las ramas remotas solamente
+```git branch -r```
--- src/camera.rs
+++ src/camera.rs
@@ -16,11 +16,32 @@ impl Camera {
         }
     }
 
-    // Método para cambiar la base: transformar la dirección de un vector desde la perspectiva de la cámara
+    // Método para orbitar la cámara (Yaw y Pitch)
+    pub fn orbit(&mut self, delta_yaw: f32, delta_pitch: f32) {
+        let radius_vector = self.eye - self.center;
+        let radius = radius_vector.magnitude();
+
+        let current_yaw = radius_vector.z.atan2(radius_vector.x);
+        let radius_xz = (radius_vector.x * radius_vector.x + radius_vector.z * radius_vector.z).sqrt();
+        let current_pitch = (-radius_vector.y).atan2(radius_xz);
+
+        let new_yaw = (current_yaw + delta_yaw) % (2.0 * std::f32::consts::PI);
+        let new_pitch = (current_pitch + delta_pitch).clamp(-std::f32::consts::PI / 2.0 + 0.1, std::f32::consts::PI / 2.0 - 0.1);
+
+        let new_eye = self.center + Vec3::new(
+            radius * new_yaw.cos() * new_pitch.cos(),
+            -radius * new_pitch.sin(),
+            radius * new_yaw.sin() * new_pitch.cos(),
+        );
+
+        self.eye = new_eye;
+    }
+
+    // Método para realizar el cambio de base (transformar un vector desde la perspectiva de la cámara)
     pub fn basis_change(&self, vector: &Vec3) -> Vec3 {
         let forward = (self.center - self.eye).normalize();     // Eje Z de la cámara (mirando hacia adelante)
         let right = forward.cross(&self.up).normalize();        // Eje X de la cámara (producto cruz)
-        let up = right.cross(&forward).normalize();             // Eje Y de la cámara (producto cruz del eje X y Z)
+        let up = right.cross(&forward).normalize();             // Eje Y de la cámara
 
         let rotated = 
             vector.x * right + 
